594226831f14c4d7e7fc4d5c707d781c19cb8e77
w88888 <www.1060067096@QQ.COM>
2017-03-09 19:43:30 +0800

    王佳一

---
+++ app/js/wjy_footer.js
@@ -0,0 +1,13 @@
+// window.onload=function(){
+// 	var wjy_btnimg=document.querySelectorAll(".wjy_btnimg")
+// 	var wjy_arr=["images/jzx/jzx-foot-move.png,images/jzx/jzx-foot-bfriend.png,images/jzx/jzx-foot-bmove.png,images/jzx/jzx-foot-bmessage.png,images/jzx/jzx-foot-bmy.png"]
+// 	console.log(wjy_arr)
+// 	// var wjy_arr1=["images/jzx/jzx-foot-bmy.png","images/jzx/jzx-foot-bfriend.png","images/jzx/jzx-foot-bmove.png","images/jzx/jzx-foot-bmessage.png","images/jzx/jzx-foot-bindex.png"]
+// 	for (var i = 0; i < wjy_btnimg.length; i++) {
+// 		wjy_btnimg.aa=i;
+// 		wjy_btnimg[i].onclick=function(){
+// 			// this.src="images/jzx/jzx-foot-move.png";
+// 			this.src=wjy_arr[this.aa];
+// 		};
+// 	};
+// }
